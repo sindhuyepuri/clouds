b16eb16eec2e75f5029375b4590759579a7cac5f
Sindhu Yepuri <sindhu@Sindhus-MBP-2.grandenetworks.net>
2022-05-12 00:49:08 -0500

    terrain shadow function

--- src/clouds/Terrain.ts
+++ src/clouds/Terrain.ts
@@ -4,6 +4,8 @@ import { Noise } from "./Noise.js";
 import { CloudsAnimation } from "./App.js";
 
 export class Terrain implements MaterialObject {
+  private width: number = 600;
+  private depth: number = 500;
   private vertices: Vec4[];
   private ind: Vec3[];
   private norms: Vec4[];
@@ -98,24 +100,16 @@ export class Terrain implements MaterialObject {
     this.norms = [];
     let incr = 1;
 
-    let width = 600;
-    let depth = 500;
+    // let width = 600;
+    // let depth = 500;
 
-    let shadows = [];
-    for (let i = 0; i < width + 1; i++) {
-      shadows.push([]);
-      for (let j = 0; j < depth + 1; j++) {
-        shadows[i].push(0);
-      }
-    }
-
-    for (let i = -width/2; i < width/2; i+=incr) {
-      for (let j = 0; j < depth; j+=incr) { // was 300
+    for (let i = -this.width/2; i < this.width/2; i+=incr) {
+      for (let j = 0; j < this.depth; j+=incr) { // was 300
         let v1 = new Vec4([i, this.getHeight(i, j), j, 1]);
         let v2 = new Vec4([i, this.getHeight(i, j + 1), j + 1, 1]);
         let v3 = new Vec4([i + 1, this.getHeight(i + 1, j + 1), j + 1, 1]);
         let v4 = new Vec4([i + 1, this.getHeight(i + 1, j), j, 1]);
-        shadows[i + width/2][j] = this.inShadow(new Vec3(v1.xyz));
+        // shadows[i + this.width/2][j] = this.inShadow(new Vec3(v1.xyz));
         this.vertices.push(v1, v2, v3, v4);
         this.norms.push(this.getNormal(i, j), this.getNormal(i, j + 1), this.getNormal(i + 1, j + 1), this.getNormal(i + 1, j));
         this.ind.push(new Vec3([indx_count, indx_count + 1, indx_count + 2]));
@@ -125,18 +119,8 @@ export class Terrain implements MaterialObject {
       }
     }
 
-    this.shadow = [];
-    for (let i = -width/2; i < width/2; i += 1) {
-      for (let j = 0; j < depth; j += 1) {
-        this.shadow.push(shadows[i + width/2][j]);
-        this.shadow.push(shadows[i + width/2][j + 1]);
-        this.shadow.push(shadows[i + 1 + width/2][j + 1]);
-        this.shadow.push(shadows[i + 1 + width/2][j]);
-      }
-    }
     // console.log("shadow", this.shadow);
-
-    this.shadowF32 = new Float32Array(this.shadow);
+    this.updateShadows();
 
     /* Flatten Position. */
     this.verticesF32 = new Float32Array(this.vertices.length*4);
@@ -151,6 +135,32 @@ export class Terrain implements MaterialObject {
     this.norms.forEach((v: Vec4, i: number) => {this.normalsF32.set(v.xyzw, i*4)});
 
     // console.log(this.normalsFlat());
+  }
+
+  public updateShadows() {
+    let shadows = [];
+    for (let i = 0; i < this.width + 1; i++) {
+      shadows.push([]);
+      for (let j = 0; j < this.depth + 1; j++) {
+        shadows[i].push(0);
+      }
+    }
+    for (let i = -this.width/2; i < this.width/2; i+=1) {
+      for (let j = 0; j < this.depth; j+=1) { // was 300
+        let v1 = new Vec4([i, this.getHeight(i, j), j, 1]);
+        shadows[i + this.width/2][j] = this.inShadow(new Vec3(v1.xyz));
+      }
+    }
+    this.shadow = [];
+    for (let i = -this.width/2; i < this.width/2; i += 1) {
+      for (let j = 0; j < this.depth; j += 1) {
+        this.shadow.push(shadows[i + this.width/2][j]);
+        this.shadow.push(shadows[i + this.width/2][j + 1]);
+        this.shadow.push(shadows[i + 1 + this.width/2][j + 1]);
+        this.shadow.push(shadows[i + 1 + this.width/2][j]);
+      }
+    }
+    this.shadowF32 = new Float32Array(this.shadow);
   }
 
   public positions(): Vec4[] {
